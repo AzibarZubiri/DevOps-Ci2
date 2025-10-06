05360c8396eac673df7b44e39aed8058c5293e37
Azibar <agonzalezme@zubirimanteo.com>
2025-10-06 14:30:01 +0200

    Segundo push

---
+++ src/main/java/actions/Calculadora.java
@@ -0,0 +1,16 @@
+package actions;
+
+public class Calculadora {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }   
+    public int multiply(int a, int b){
+        return a * b;
+    }
+    
+}
